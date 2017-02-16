e22813070fe9b14b72611ea27e555bff93785bcc
Ryuuz <cmodegaard@gmail.com>
2017-02-16 11:01:41 +0100

    Prototype
    First prototype with walking!

---
+++ Source/Prototype/MyCharacter.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prototype.h"
+#include "MyCharacter.h"
+
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+	if (!CurrentVelocity.IsZero())
+	{
+		FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+		SetActorLocation(NewLocation);
+	}
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	InputComponent->BindAxis("MoveX", this, &AMyCharacter::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AMyCharacter::Move_YAxis);
+}
+
+void AMyCharacter::Move_XAxis(float AxisValue)
+{
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.f, 1.f) * 100.f;
+}
+
+void AMyCharacter::Move_YAxis(float AxisValue)
+{
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.f, 1.f) * 100.f;
+}
